0900bba0a6099a5ae794504519b3e66943b7dc12
Sean Zhang <xinyuzhang@google.com>
2014-08-15 22:25:41 -0700

    Some update in Section 3.5

--- Book_SinglePageWebApplications/spa/js/spa.shell.js
+++ Book_SinglePageWebApplications/spa/js/spa.shell.js
@@ -13,12 +13,16 @@ spa.shell = (function() {
         	+ '</div>'
         	+ '<div class="spa-shell-foot"></div>'
         	+ '<div class="spa-shell-chat"></div>'
-        	+ '<div class="spa-shell-modal"></div>'
+        	+ '<div class="spa-shell-modal"></div>',
+        chat_extend_time: 1000,
+        chat_retract_time: 300,
+        chat_extend_height: 450,
+        chat_retract_height: 15
 	},
 	stateMap = {$container: null},
 	jqueryMap = {},
 
-	setJqueryMap, initModule;
+	setJqueryMap, toggleChat, initModule;
 
 	setJqueryMap = function () {
 		var $container = stateMap.$container;
